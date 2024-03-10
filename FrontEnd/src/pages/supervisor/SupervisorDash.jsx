09f7e5765e00c2dd57ef3a3ca943cbb226e56e74
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-03-10 16:02:10 +0530

    Update SupervisorDash.jsx

--- FrontEnd/src/pages/supervisor/SupervisorDash.jsx
+++ FrontEnd/src/pages/supervisor/SupervisorDash.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import TopNav from "../../components/TopNav";
-<<<<<<< HEAD
-=======
-import Sidebar from "../../components/Sidebar";
->>>>>>> 3e7fb9ee920122f5d47fbb96b479a9da9f7e9f1a
 
 function SupervisorDash () {
 
@@ -13,10 +9,6 @@ function SupervisorDash () {
 
         
         <TopNav />
-<<<<<<< HEAD
-=======
-        <Sidebar/>
->>>>>>> 3e7fb9ee920122f5d47fbb96b479a9da9f7e9f1a
         
         </>
     );
