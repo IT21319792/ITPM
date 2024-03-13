245b2416ba5910a04c6320cf14d06209dfc9e524
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-14 02:42:06 +0530

    feat(route) :create search route for schedules

--- Backend/routes/SchedulePresentationRoutes.js
+++ Backend/routes/SchedulePresentationRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import  {createSchedule,getSchedules,updateSchedule,deleteSchedule} from '../controllers/SchedulePresentationController.js';
+import  {createSchedule,getSchedules,updateSchedule,deleteSchedule,searchSchedule} from '../controllers/SchedulePresentationController.js';
 const SchduleRouter = express.Router();
 
 SchduleRouter.post('/createSchedule', createSchedule);
 SchduleRouter.get('/getSchedules', getSchedules);
 SchduleRouter.put('/putSchedule/:id', updateSchedule);
 SchduleRouter.delete('/deleteSchedule/:id', deleteSchedule);
-
+SchduleRouter.get('/searchSchedule/:key', searchSchedule);
 
 export default SchduleRouter;
