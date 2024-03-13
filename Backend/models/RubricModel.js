329b5c4f1517d127fd88f104c7d3ef875be811c3
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-13 20:52:53 +0530

    chore(model) : change rubric model from common js to ECMAScript 6

--- Backend/models/RubricModel.js
+++ Backend/models/RubricModel.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 const RubricSchema = new Schema({
     rubricID: {type: String, required: true, unique: true},
+    topic: {type: String, required: true},
     criteria: {type: String, required: true},
     marks: {type: Number, required: true},
     type: {type: String, required: true}
 }, {timestamps: true});
 
 const RubricModel = mongoose.model('rubrics', RubricSchema);
-module.exports = RubricModel;
+export default RubricModel;
