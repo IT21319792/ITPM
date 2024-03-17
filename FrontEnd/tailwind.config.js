8220cdda328a4e538b7b813f4674705badf195c3
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-17 17:50:46 +0530

    chore(tailwind) : change theme colors

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightWhite: "#F2F4F8"
+      },
+    },
   },
   plugins: [],
 }
