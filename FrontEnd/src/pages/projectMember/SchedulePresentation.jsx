d630b54361eaac251d384b98645db5f4fd25e78b
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-17 16:39:55 +0530

    feat(page) :create add schedule page

---
+++ FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from 'react';
+
+function SchedulePresentation() {
+    return (
+        <h1>Schedule Presentation</h1>
+    )
+}
+
+export default SchedulePresentation;
