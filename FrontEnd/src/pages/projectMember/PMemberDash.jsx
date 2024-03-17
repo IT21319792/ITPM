22f7bca1a113075b15aa987d1fb3df0b3eef019c
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-17 16:46:09 +0530

    fix(path) : fix navigation path

--- FrontEnd/src/pages/projectMember/PMemberDash.jsx
+++ FrontEnd/src/pages/projectMember/PMemberDash.jsx
@@ -7,7 +7,7 @@ function PMemberDash() {
     return (
         <>
             <PMemberWelcomeard />
-            <Link to="/dashboard/pMemberDash/SchedulePresentation">
+            <Link to="/pMemberDash/SchedulePresentation">
             <button type="button" className="btnSchedule bg-green-800 text-white font-bold rounded-md px-4 py-2">
                 Schedule Presentation
             </button>
