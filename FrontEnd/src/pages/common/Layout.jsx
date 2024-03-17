0dfb52da19d297f1ada09eb418b7b07f7cc3312d
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-17 17:49:40 +0530

    style(outlet) :change outlet colour

--- FrontEnd/src/pages/common/Layout.jsx
+++ FrontEnd/src/pages/common/Layout.jsx
@@ -1,57 +1,59 @@
-import * as React from 'react';
-import BasicLineChart from '../../components/BasicLineChart';
-import TopNav from '../../components/TopNav';
-import WelcomeCard from '../../components/WelcomeCard';
-import Sidebar from '../../components/Sidebar';
-import { Outlet } from 'react-router-dom';
-import { adminListItems, studentListItems, examinarListItems, supervisorListItems, memberListItems } from '../../components/NavNavigation/listItems';
-import { useEffect } from 'react';
-import Cookies from 'js-cookie';
+import * as React from "react";
+import BasicLineChart from "../../components/BasicLineChart";
+import TopNav from "../../components/TopNav";
+import WelcomeCard from "../../components/WelcomeCard";
+import Sidebar from "../../components/Sidebar";
+import { Outlet } from "react-router-dom";
+import {
+  adminListItems,
+  studentListItems,
+  examinarListItems,
+  supervisorListItems,
+  memberListItems,
+} from "../../components/NavNavigation/listItems";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 export default function Layout() {
+  const userRole = Cookies.get("role");
+  const [navLinks, setNavlinks] = React.useState();
 
-    const userRole=Cookies.get('role');
-    const [navLinks,setNavlinks]=React.useState()
-    
+  useEffect(() => {
+    switch (userRole) {
+      case "admin": //Admin
+        setNavlinks(adminListItems);
+        break;
+      case "student": //Student
+        setNavlinks(studentListItems);
+        break;
+      case "examinar": //Examinar
+        setNavlinks(examinarListItems);
+        break;
+      case "supervisor": //Supervisor
+        setNavlinks(supervisorListItems);
+        break;
+      case "member": //Project Member
+        setNavlinks(memberListItems);
+        break;
 
-    useEffect(() => {
-        switch (userRole) {
-          case "admin": //Admin
-            setNavlinks(adminListItems);
-            break;
-          case "student": //Student
-            setNavlinks(studentListItems);
-            break;
-          case "examinar": //Examinar
-            setNavlinks(examinarListItems);
-            break;
-          case "supervisor": //Supervisor
-            setNavlinks(supervisorListItems);
-            break;
-          case "member": //Project Member
-            setNavlinks(memberListItems);
-            break;
-          
-          default:          
-            toast.error("Login Expired ! Please Login Again");
-            navigate("/");
-        }
-    
-      }, []);
+      default:
+        toast.error("Login Expired ! Please Login Again");
+        navigate("/");
+    }
+  }, []);
 
+  return (
+    <>
+      <Sidebar navLinks={navLinks} />
 
-    return (
-        <>
-            <Sidebar navLinks={navLinks}/>
-            
-            <div style={{ marginLeft: '240px' }}> {/* Adjust margin based on Sidebar width */}
-            
-                <TopNav />
-                <div style={{ marginTop: '10px', marginLeft:'10px'}}>
-                <Outlet />
-                </div>
-            </div>
-        </>
-    );
+      <div style={{ marginLeft: "240px" }}>
+        {" "}
+        {/* Adjust margin based on Sidebar width */}
+        <TopNav />
+        <div className="h-screen bg-lightWhite">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
-
