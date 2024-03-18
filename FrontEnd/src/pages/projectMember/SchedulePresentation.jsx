0f1d1e31b1c28ea75bf8e481905cca26a1cf28dc
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-18 13:52:32 +0530

    feat(form) : addeed location and time feild to schedule form

--- FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
+++ FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
@@ -48,6 +48,41 @@ function SchedulePresentation() {
                   </label>
                 </div>
               </div>
+              <div className="grid grid-cols-2">
+                <div className="col">
+                  <label class="block text-sm font-medium text-slate-500" htmlFor="group ">Location</label>
+                  <input
+                      type="text" placeholder="Enter Location"
+                      class="block min-w-[425px] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                     invalid:border-pink-500 invalid:text-pink-600
+                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    />
+                    <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                      Please provide a location.
+                    </p>
+                </div>
+                <div className="col">
+                  <label class="block">
+                    <span class="block text-sm font-medium text-slate-500">
+                      Time
+                    </span>
+                    <input
+                      type="text" placeholder="HH:MM AM/PM"
+                      class="block min-w-[425px] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                     invalid:border-pink-500 invalid:text-pink-600
+                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    />
+                    <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                      Please provide the time.
+                    </p>
+                  </label>
+                </div>
+              </div>
+              
             </form>
           </div>
         </div>
