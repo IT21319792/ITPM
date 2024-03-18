143f1313333382b8f3d4f21c1800ec5cedc6a0c6
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-18 11:51:58 +0530

    feat(group) : added grp feild

--- FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
+++ FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
@@ -24,7 +24,25 @@ function SchedulePresentation() {
                     </select>
                   </div>
                 </div>
-                
+                <div className="col">
+
+                  <label class="block">
+                    <span class="block text-sm font-medium text-slate-500">
+                      Select Date
+                    </span>
+                    <input
+                      type="date"
+                      class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                     invalid:border-pink-500 invalid:text-pink-600
+                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    />
+                    <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                      Please provide a valid date address.
+                    </p>
+                  </label>
+                </div>
               </div>
             </form>
           </div>
