50d11f9eee0d5f0be7d4c4972617bd09d20ae981
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-20 07:48:42 +0530

    feat(SPValidation) : create variable to access feilds

---
+++ FrontEnd/src/validation/SchedulePresentation.jsx
@@ -0,0 +1,17 @@
+import validator from 'validator';
+
+const validateSchedulePresentation = (data) => {
+    let errors = {};
+    const{
+        ScheduleID,
+        GroupID,
+        date,
+        timeDuration,
+        location,
+        topic,
+        examiners
+       }
+
+    
+};
+export default validateSchedulePresentation;
