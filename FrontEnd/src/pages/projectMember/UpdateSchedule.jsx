c532d74b9aab8d603e6d9cf4f12ed65be134b1a4
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-20 16:54:19 +0530

    feat(US) : initialized update schedule page

---
+++ FrontEnd/src/pages/projectMember/UpdateSchedule.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PMemberWelcomeard from "../../components/PMemberWelcomeCard";
+import { Link } from "react-router-dom";
+
+function PMemberDash() {
+  return (
+    <h1>hi</h1>
+
+  );
+}
+export default PMemberDash;
