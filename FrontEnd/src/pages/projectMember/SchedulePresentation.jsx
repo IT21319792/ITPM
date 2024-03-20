18da1ba2edf760f2b987194965508b658db69e32
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-20 08:30:14 +0530

    feat(SP) : set date

--- FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
+++ FrontEnd/src/pages/projectMember/SchedulePresentation.jsx
@@ -113,7 +113,7 @@ function SchedulePresentation(props) {
                       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                      invalid:border-pink-500 invalid:text-pink-600
-                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                     focus:invalid:border-pink-500 focus:invalid:ring-pink-500" value={date} onChange={(e) => {setDate(e.target.value)}}
                       min={new Date().toISOString().split("T")[0]}
                     />
                     <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
