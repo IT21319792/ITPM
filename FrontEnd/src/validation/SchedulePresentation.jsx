348d2cd628fa9fa06420664ab064f9c0f7633cc4
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-20 07:49:41 +0530

    feat(SPValidation) : validate schecule presentation

--- FrontEnd/src/validation/SchedulePresentation.jsx
+++ FrontEnd/src/validation/SchedulePresentation.jsx
@@ -12,6 +12,17 @@ const validateSchedulePresentation = (data) => {
         examiners
        }
 
+       if(validator.isEmpty(ScheduleID)){
+        errors.ScheduleID = "ScheduleID is required";
+    }else if(validator.isInt(ScheduleID)){
+        errors.ScheduleID = "ScheduleID should not be integer";
+    }else if{
+        const scheduleIDPattern = /^SP[0-9]+$/;
+        IdleDeadline(scheduleIDPattern.test(ScheduleID) == false){
+            errors.ScheduleID = "ScheduleID should be in the format SPXXXX";
+        }
+    }
+
     
 };
 export default validateSchedulePresentation;
