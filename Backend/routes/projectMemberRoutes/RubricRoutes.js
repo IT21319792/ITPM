0686e367be235170a771e5f0dd3964da93c42fb7
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-21 22:19:55 +0530

    feat(rubricRoute)  added search by ID route

--- Backend/routes/projectMemberRoutes/RubricRoutes.js
+++ Backend/routes/projectMemberRoutes/RubricRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteRubric,
   searchRubricByTopic,
   searchRubricByType,
+  searchRubricByRubricID,
 } from "../../controllers/projectMemberControllers/RubricController.js";
 const RubricRouter = express.Router();
 
@@ -15,5 +16,6 @@ RubricRouter.put("/putrubric/:id", updateRubric);
 RubricRouter.delete("/deleterubric/:id", deleteRubric);
 RubricRouter.get("/searchrubrictype/:key", searchRubricByType);
 RubricRouter.get("/searchrubrictopic/:key", searchRubricByTopic);
+RubricRouter.get("/searchrubricbyid/:key", searchRubricByRubricID);
 
 export default RubricRouter;
