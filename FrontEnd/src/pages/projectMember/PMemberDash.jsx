c66cd7f5e559e64df4228c0a6641041e8d72f2d3
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-22 18:49:06 +0530

    feat(PM) : change button names

--- FrontEnd/src/pages/projectMember/PMemberDash.jsx
+++ FrontEnd/src/pages/projectMember/PMemberDash.jsx
@@ -12,9 +12,9 @@ function PMemberDash() {
         <Link to="/dashboard/pMemberDash/SchedulePresentation">
             <button
                 type="button"
-                className="btnSchedule bg-blue-600 text-white font-bold rounded-md px-4 py-2 mb-3"
+                className="btnSchedule bg-blue-800 text-white font-bold rounded-md px-4 py-2 mb-3"
             >
-                Publish Schedule
+                Create Schedule
             </button>
         </Link>
         <Link to="/dashboard/pMemberDash/ScheduledPresentations">
@@ -22,7 +22,7 @@ function PMemberDash() {
                 type="button"
                 className="btnSchedule bg-green-800 text-white font-bold rounded-md px-4 py-2 ml-20 mr-20"
             >
-                Scheduled Presentation
+               Schedueled List  
             </button>
         </Link>
     </div>
