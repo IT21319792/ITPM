20851f0d73bbb82aea2586b79ffd262162ed77c3
IndudiniThennakoon <thennakoonindudini@gmail.com>
2024-03-22 17:04:30 +0530

    feat(SV): chnage scheudle presnetation vlidation

--- FrontEnd/src/validation/SchedulePresentation.jsx
+++ FrontEnd/src/validation/SchedulePresentation.jsx
@@ -40,17 +40,30 @@ const SchedulePresentationValidation = (data) => {
       /^(0?[1-9]|1[0-2]):([0-5][0-9])\s(?:AM|PM)\s-\s(0?[1-9]|1[0-2]):([0-5][0-9])\s(?:AM|PM)$/;
     if (pattern.test(timeDuration) === false) {
       errors.timeDuration =
-        "Please provide a valid time duration (e.g., 08:30 AM - 09:00 AM";
+        "Please provide a valid time duration (e.g., 08:30 AM - 09:00 AM)";
     }
   }
-  if (validator.isEmpty(location)) {
-    errors.location = "Location is required";
-  } else {
-    const pattern = /^[a-zA-Z0-9\s,.'-]{4,}$/;
+
+  // location should start with capital letter with 3 more integeres only maximum size should be 4
+  if (location.length > 4) {
+    errors.location = "Location should not be more than 4 characters";
+  } else{
+    const pattern = /^[A-Z]{1}[0-9]{3}$/;
     if (pattern.test(location) === false) {
-      errors.location = "Please provide a valid location";
+      errors.location = "Please provide a valid location (e.g., A123)";
     }
   }
+
+  // if (validator.isEmpty(location)) {
+  //   errors.location = "Location is required";
+  // } else if (validator.isInt(location)) {
+  //   errors.location = "Location should not be an integer";
+  // }else{
+  //   const pattern = /^[a-zA-Z0-9\s,.'-]{4,}$/;
+  //   if (pattern.test(location) === false) {
+  //     errors.location = "Please provide a valid location";
+  //   }
+  // }
 
   if (validator.isEmpty(topic)) {
     errors.topic = "Topic is required";
