0cba6d656e353bbd4b02c5052d2a292ae32e693d
IT21158186 <it21158186@my.sliit.lk>
2024-03-23 16:56:32 +0530

    implement the search function to myResults page

--- FrontEnd/src/components/TopNav.jsx
+++ FrontEnd/src/components/TopNav.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import MenuIcon from '@mui/icons-material/Menu';
 const TopNav = () => {
