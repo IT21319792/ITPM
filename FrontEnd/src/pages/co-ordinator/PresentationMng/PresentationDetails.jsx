7dc077f196e55f5c1f0f3444c7fad56f57eed43b
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-08-29 07:03:37 +0530

    Update PresentationDetails.jsx

--- FrontEnd/src/pages/co-ordinator/PresentationMng/PresentationDetails.jsx
+++ FrontEnd/src/pages/co-ordinator/PresentationMng/PresentationDetails.jsx
@@ -95,8 +95,11 @@ function PresentationDetails() {
                 </td>
                 <td className="p-2 border border-gray-300">
                   <button onClick={() => handleUpdateClick(schedule)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline">
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 4v7a4 4 0 01-4 4H4" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 20h8" /></svg>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-5m-4.586-7.586a2 2 0 112.828 2.828L11 14l-4 1 1-4 6.414-6.414z" />
+                    </svg>
                   </button>
+
                 </td>
               </tr>
             ))}
@@ -145,7 +148,7 @@ function PresentationDetails() {
                 className="border border-gray-300 p-2 rounded"
               />
             </div>
-            
+
             <div className="flex flex-col mb-4">
               <label className="mb-1">Time Duration:</label>
               <div className="flex items-center">
